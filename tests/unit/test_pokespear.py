0423e224f298ce5f566074d8149ed39fbea982bb
Daniel Brownridge <daniel@freshnewpage.com>
2020-05-06 06:50:29 +0100

    Add unit tests for command line function.

---
+++ tests/unit/test_pokespear.py
@@ -0,0 +1,9 @@
+"""
+Unit tests for the pokespear script.
+"""
+from pokespear.command_line import main
+
+
+def test_main_callable():
+    """Check that the entry point for the pokespear command-line script can be called."""
+    main()
