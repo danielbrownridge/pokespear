9e47a14dee12777ef04ddc3b9b4dcfc72e846de6
Daniel Brownridge <daniel@freshnewpage.com>
2020-05-07 20:28:48 +0100

    Convert to idomatic import

--- tests/unit/test_pokespear.py
+++ tests/unit/test_pokespear.py
@@ -1,12 +1,12 @@
 """
 Unit tests for the pokespear script.
 """
-from pytest import raises
+import pytest
 
 from pokespear.command_line import main
 
 
 def test_main_callable() -> None:
     """Check entry point for pokespear command-line script can be called."""
-    with raises(SystemExit):
+    with pytest.raises(SystemExit):
         main()
