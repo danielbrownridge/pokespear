4fae2bf3683de3623825ab5ca415768e7cc10117
Daniel Brownridge <daniel@freshnewpage.com>
2020-05-07 11:52:31 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name="pokespear",
+    package=[
+        ""
+    ],
+    package_dir={
+        "": "src"
+    },
+    use_scm_version=True,
+    setup_requires=[
+        "setuptools_scm",
+    ]
+)
