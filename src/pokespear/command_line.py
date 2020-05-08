703c0a91d34dbcbd471e352e0129438908e1b56e
Daniel Brownridge <daniel@freshnewpage.com>
2020-05-08 09:04:20 +0100

    Add required positional argument to command line options

--- src/pokespear/command_line.py
+++ src/pokespear/command_line.py
@@ -2,15 +2,16 @@
 pokespear:
 Command line script to display Shakesperian descriptions of Pokémon.
 """
-from argparse import ArgumentParser
-import os
-import sys
+import argparse
+import email
+import pkg_resources
 
 
 def main() -> None:
     """Entry point for pokespear command line script."""
-    parser = ArgumentParser(add_help=False)
+    dist = pkg_resources.get_distribution(__package__)
+    metadata = email.message_from_string(dist.get_metadata(dist.PKG_INFO))
+    description = metadata["summary"]
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("pokemon")
     parser.parse_args()
-    if len(sys.argv) == 1:
-        parser.print_usage()
-        sys.exit(os.EX_USAGE)
--- tests/functional/test_pokespear.py
+++ tests/functional/test_pokespear.py
@@ -17,5 +17,9 @@ def test_no_args() -> None:
     """Check a usage message is displayed if no arguments are supplied"""
     args = "pokespear"
     with pytest.raises(CalledProcessError) as execinfo:
-        run(args, check=True, encoding="utf-8", stdout=PIPE)
-    assert execinfo.value.stdout == "usage: pokespear\n"
+        run(args, check=True, encoding="utf-8", stdout=PIPE, stderr=PIPE)
+    assert execinfo.value.stdout == ''
+    assert execinfo.value.stderr == (
+        "usage: pokespear [-h] pokemon\n"
+        "pokespear: error: the following arguments are required: pokemon\n"
+    )
