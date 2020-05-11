c1d3f6794aea7555d2993bf6eb6aff2620faae92
Daniel Brownridge <daniel@freshnewpage.com>
2020-05-11 16:36:21 +0100

    No mypy on tests, parameterized test.

--- tests/functional/test_pokespear.py
+++ tests/functional/test_pokespear.py
@@ -20,14 +20,14 @@ OPTIONAL_ARGUMENTS = (
 )
 
 
-def test_script_found() -> None:
+def test_script_found():
     """Check that pokespear exists and is executable."""
     args = "pokespear"
     with pytest.raises(CalledProcessError):
         run(args, check=True)
 
 
-def test_no_args() -> None:
+def test_no_args():
     """Check a usage message is displayed if no arguments are supplied"""
     args = "pokespear"
     with pytest.raises(CalledProcessError) as execinfo:
@@ -39,7 +39,7 @@ def test_no_args() -> None:
     )
 
 
-def test_help() -> None:
+def test_help():
     """Check that a help message is displayed when the '-h' option is used"""
     args = ["pokespear", "-h"]
 
@@ -54,7 +54,7 @@ def test_help() -> None:
     assert completed.stderr == ''
 
 
-def test_bad_pokemon() -> None:
+def test_bad_pokemon():
     """Check an error message is displayed if an invalid Pokémon is given."""
     args = ["pokespear", "banana"]
     error = "resource not found (banana), check spelling\n"
--- tests/unit/test_pokespear.py
+++ tests/unit/test_pokespear.py
@@ -6,29 +6,29 @@ import pytest
 from pokespear.command_line import main, get_pokemon_description
 
 
-def test_main_callable() -> None:
+def test_main_callable():
     """Check entry point for pokespear command-line script can be called."""
     with pytest.raises(SystemExit):
         main()
 
 
-def test_getpokemondescription_success() -> None:
-    """A text description should be returned when the name of a pokémon is
-    supplied.
-    """
-    pokemon = "bulbasaur"
-    description = (
+@pytest.mark.parametrize("pokemon, description", [
+    ("bulbasaur", (
         "A strange seed was\n"
         "planted on its\n"
         "back at birth.\f"
         "The plant sprouts\n"
         "and grows with\n"
-        "this POKéMON."
-    )
+        "this POKéMON.")),
+])
+def test_getpokemondescription_success(pokemon, description):
+    """A text description should be returned when the name of a pokémon is
+    supplied.
+    """
     assert get_pokemon_description(pokemon) == description
 
 
-def test_getpokemondescription_failure() -> None:
+def test_getpokemondescription_failure():
     """A ValueError should be raised if an invalid pokemon is given."""
     pokemon = "notapokemon"
     exception = "resource not found (notapokemon), check spelling"
