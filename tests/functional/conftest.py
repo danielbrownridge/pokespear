bc977bdcbf75882c45c3dfefcae6c507b41e44f7
Daniel Brownridge <daniel@freshnewpage.com>
2020-05-14 08:52:38 +0100

    Add docker container

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -15,8 +15,11 @@ def http():
 def service_url(function_scoped_container_getter):
     """The url of the service from a running container."""
     service_name = "pokespear"
-    container = function_scoped_container_getter(service_name)
+    container = function_scoped_container_getter.get(service_name)
+    network_info = container.network_info[0]
     url = urllib3.util.url.Url(
-        scheme="http"
+        scheme="http",
+        host=network_info.hostname,
+        port=network_info.host_port
     )
     return url
